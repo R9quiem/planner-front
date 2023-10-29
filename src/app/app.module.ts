558f7a5d8706b9ea221536be2f4d152af4f369e3
efim <efimburachevsky@gmail.com>
2023-10-29 17:06:38 +0300

    добавил компонент для задач

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './views/auth/auth.component';
+import { TaskListComponent } from './views/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
