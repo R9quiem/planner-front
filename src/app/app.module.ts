9507362159e9ec7e5937f9b257372f9f51eb2a16
efim <efimburachevsky@gmail.com>
2023-10-29 13:50:36 +0300

    добавил пустые component и service для регистрации/входа (auth)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
+import { AuthComponent } from './views/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule
