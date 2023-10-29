2153d3675287de5d21b8c71a43a742bd76e7702e
efim <efimburachevsky@gmail.com>
2023-10-29 13:02:14 +0300

    немного рефакторю

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,5 +14,5 @@ import { HomeComponent } from './home/home.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
+  })
+  export class AppModule { }
