7d24f9906763648fdf7d17c909a2860373253b73
anya <anna69kl@gmail.com>
2023-11-13 01:43:43 +0300

    работала над компонентом app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LogInComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
