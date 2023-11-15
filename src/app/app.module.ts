72d63d1040ae9e5d8f418ce20d3274b6a3f6aa2a
efim <efimburachevsky@gmail.com>
2023-11-15 17:55:41 +0300

    правки в home comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AuthComponent } from './core/auth/auth.component';
-import { TaskListComponent } from './features/task-list/task-list.component';
+import { AuthComponent} from "./core/auth/auth.component";
+import { TaskListComponent} from "./features/task-list/task-list.component";
 import { HomeComponent } from './features/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from "./app-routing.module";
+import { MainPageComponent } from './features/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     TaskListComponent,
-    HomeComponent
+    HomeComponent,
+    MainPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  imports: [ BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
-  })
-  export class AppModule { }
+})
+export class AppModule { }
