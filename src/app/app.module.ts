52ce14c3357e82a85a5f69c734bcdfd53de19cc5
efim <efimburachevsky@gmail.com>
2023-11-19 04:26:14 +0300

    правки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AuthComponent } from './core/auth/auth.component';
 import { TaskListComponent } from './features/main-page/task-list.component';
 import { HomeComponent } from './features/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// import { MatCardModule } from '@angular/material/card';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     TaskListComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatCardModule,
+    // MatFormFieldModule,
+    // MatInputModule,
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
