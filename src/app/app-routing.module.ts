a4dd1d3344e78b105d3894567097c8704fe1782a
anya <anna69kl@gmail.com>
2023-11-22 22:40:27 +0300

    правки в home comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@
   import {HomeComponent} from "./features/home/home.component";
   import {AuthComponent} from "./core/auth/auth.component";
   import {TaskListComponent} from "./features/task-list/task-list.component";
+  import {MainPageComponent} from "./features/main-page/main-page.component";
 
   const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'auth', component: AuthComponent },
     { path: 'tasks', component: TaskListComponent },
+    { path: 'main', component: MainPageComponent },
+    { path: 'task', component: TaskListComponent }
     // добавьте больше маршрутов здесь
   ];
 
