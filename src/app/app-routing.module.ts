6bd6d434055a09c3eb40ee9c0c62e3d5647505b6
anya <anna69kl@gmail.com>
2023-11-25 11:03:36 +0300

    правки в home comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@
   import {AuthComponent} from "./core/auth/auth.component";
   import {TaskListComponent} from "./features/task-list/task-list.component";
   import {MainPageComponent} from "./features/main-page/main-page.component";
+  import {TodayComponent} from "./features/task-list/today/today.component";
+  import {ProjectComponent} from "./features/task-list/project/project.component";
 
   const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'auth', component: AuthComponent },
     { path: 'tasks', component: TaskListComponent },
     { path: 'main', component: MainPageComponent },
-    { path: 'task', component: TaskListComponent }
+    { path: 'task', component: TaskListComponent },
+    { path: 'today', component: TodayComponent },
+    { path: 'project', component: ProjectComponent }
     // добавьте больше маршрутов здесь
   ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,14 @@ import { MainPageComponent } from './features/main-page/main-page.component';
 import { MenuComponent } from './features/main-page/menu/menu.component';
 import {RegisterComponent} from "./core/auth/register/register.component";
 import {LoginComponent} from "./core/auth/login/login.component";
+import { HeaderComponent } from './features/main-page/header/header.component';
+import { TodayComponent } from './features/task-list/today/today.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import { AllTasksComponent } from './features/task-list/all-tasks/all-tasks.component';
+import { CalenderComponent } from './features/task-list/calender/calender.component';
+import { ProjectComponent } from './features/task-list/project/project.component';
+import { BackgroundComponent } from './features/main-page/background/background.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +29,12 @@ import {FormsModule} from "@angular/forms";
     HomeComponent,
     MainPageComponent,
     MenuComponent,
+    HeaderComponent,
+    TodayComponent,
+    AllTasksComponent,
+    CalenderComponent,
+    ProjectComponent,
+    BackgroundComponent,
   ],
   imports: [
     BrowserModule,
