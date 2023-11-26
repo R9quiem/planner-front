ecab309563e70d031ba40c66a27d7bddbbe57e52
efim <efimburachevsky@gmail.com>
2023-11-26 23:59:37 +0300

    супер большие правки всего

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@
   import {Routes, RouterModule, PreloadAllModules} from "@angular/router";
   import {HomeComponent} from "./features/home-page/home.component";
   import {AuthComponent} from "./core/auth/auth.component";
-  import {TaskListComponent} from "./features/task-list/task-list.component";
+  import {TaskListComponent} from "./features/task-list (не используется)/task-list.component";
   import {MainPageComponent} from "./features/main-page/main-page.component";
-  import {TodayComponent} from "./features/task-list/today-tasks-component/today.component";
-  import {ProjectComponent} from "./features/task-list/categories-component/project.component";
+  import {TodayComponent} from "./features/main-page/today-tasks-component/today.component";
+  import {ProjectComponent} from "./features/main-page/categories-component/project.component";
   import {HeaderComponent} from "./features/main-page/shared/header/header.component";
   import {AllTasksComponent} from "./features/main-page/all-tasks-component/all-tasks.component";
-  import {CalenderComponent} from "./features/task-list/calender-component/calender.component";
+  import {CalenderComponent} from "./features/main-page/calender-component/calender.component";
   import {SidebarComponent} from "./features/main-page/shared/sidebar/sidebar.component";
 
   const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthComponent} from "./core/auth/auth.component";
-import { TaskListComponent} from "./features/task-list/task-list.component";
+import { TaskListComponent} from "./features/task-list (не используется)/task-list.component";
 import { HomeComponent } from './features/home-page/home.component';
 import { AppRoutingModule} from "./app-routing.module";
 import { MainPageComponent } from './features/main-page/main-page.component';
@@ -11,12 +11,12 @@ import { SidebarComponent } from './features/main-page/shared/sidebar/sidebar.co
 import {RegisterComponent} from "./core/auth/register/register.component";
 import {LoginComponent} from "./core/auth/login/login.component";
 import { HeaderComponent } from './features/main-page/shared/header/header.component';
-import { TodayComponent } from './features/task-list/today-tasks-component/today.component';
+import { TodayComponent } from './features/main-page/today-tasks-component/today.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { AllTasksComponent } from './features/main-page/all-tasks-component/all-tasks.component';
-import { CalenderComponent } from './features/task-list/calender-component/calender.component';
-import { ProjectComponent } from './features/task-list/categories-component/project.component';
+import { CalenderComponent } from './features/main-page/calender-component/calender.component';
+import { ProjectComponent } from './features/main-page/categories-component/project.component';
 import { BackgroundComponent } from './features/main-page/shared/background/background.component';
 import {AuthInterceptor} from "./core/auth/util/AuthInterceptor";
 
